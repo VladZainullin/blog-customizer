6562c821489f74444b160575e9e4af1af21716fd
Vladislav Zainullin <mittoface02@mail.ru>
2026-02-16 20:38:41 +0300

    feat(ArticleParamsForm): Поднимает состояние страницы на уровень страницы

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from 'components/article';
@@ -11,15 +11,58 @@ import {
 	fontColors,
 	fontFamilyOptions,
 	fontSizeOptions,
+	OptionType,
 } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [selectedFontFamilyOption, setSelectedFontFamilyOption] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+
+	const [selectedFontSizeOption, setSelectedFontSizeOption] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+
+	const [selectedFontColorOption, setSelectedFontColorOption] = useState(
+		defaultArticleState.fontColor
+	);
+
+	const [selectedBackgroundColorOption, setSelectedBackgroundColorOption] =
+		useState(defaultArticleState.backgroundColor);
+
+	const [selectedContentWidthOption, setSelectedContentWidthOption] = useState(
+		defaultArticleState.contentWidth
+	);
+
+	const onChangeSelectedFontFamily = (value: OptionType) => {
+		setSelectedFontFamilyOption(value);
+	};
+
+	const onChangeSelectedFontSize = (value: OptionType) => {
+		setSelectedFontSizeOption(value);
+	};
+
+	const onChangeSelectedFontColor = (value: OptionType) => {
+		setSelectedFontColorOption(value);
+	};
+
+	const onChangeSelectedBackgroundColor = (value: OptionType) => {
+		setSelectedBackgroundColorOption(value);
+	};
+
+	const onChangeSelectedContentWidth = (value: OptionType) => {
+		setSelectedContentWidthOption(value);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
@@ -32,18 +75,40 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm
-				selectedFontFamilyClass={defaultArticleState.fontFamilyOption}
-				fontFamilyClasses={fontFamilyOptions}
-				selectedFontSize={defaultArticleState.fontSizeOption}
-				fontSizes={fontSizeOptions}
-				selectedFontColor={defaultArticleState.fontColor}
-				fontColors={fontColors}
-				selectedBackgroundColor={defaultArticleState.backgroundColor}
-				backgroundColors={backgroundColors}
-				selectedContentWidth={defaultArticleState.contentWidth}
-				contentWidths={contentWidthArr}
-			/>
+			<ArticleParamsForm>
+				<Select
+					title={'Шрифт'}
+					selected={selectedFontFamilyOption}
+					options={fontFamilyOptions}
+					onChange={onChangeSelectedFontFamily}
+				/>
+				<RadioGroup
+					title={'Размер шрифта'}
+					name={'Размер шрифта'}
+					options={fontSizeOptions}
+					selected={selectedFontSizeOption}
+					onChange={onChangeSelectedFontSize}
+				/>
+				<Select
+					title={'Цвет фона'}
+					selected={selectedFontColorOption}
+					options={fontColors}
+					onChange={onChangeSelectedFontColor}
+				/>
+				<Separator />
+				<Select
+					title={'Цвет фона'}
+					selected={selectedBackgroundColorOption}
+					options={backgroundColors}
+					onChange={onChangeSelectedBackgroundColor}
+				/>
+				<Select
+					title={'Ширина контента'}
+					selected={selectedContentWidthOption}
+					options={contentWidthArr}
+					onChange={onChangeSelectedContentWidth}
+				/>
+			</ArticleParamsForm>
 			<Article />
 		</main>
 	);
